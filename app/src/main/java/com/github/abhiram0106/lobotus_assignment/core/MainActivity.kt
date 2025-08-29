44e5b1a28ac834f313f12368b9b371c5109b4a38
Abhiram <abhiram090601@gmail.com>
2025-08-29 17:57:24 +0530

    chore: init

---
+++ app/src/main/java/com/github/abhiram0106/lobotus_assignment/core/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.github.abhiram0106.lobotus_assignment.core
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.github.abhiram0106.lobotus_assignment.core.ui.theme.Lobotus_assignmentTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Lobotus_assignmentTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Lobotus_assignmentTheme {
+        Greeting("Android")
+    }
+}
---
+++ app/src/main/java/com/github/abhiram0106/lobotus_assignment/core/MyApp.kt
@@ -0,0 +1,9 @@
+package com.github.abhiram0106.lobotus_assignment.core
+
+import android.app.Application
+
+class MyApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
